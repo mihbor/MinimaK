8d8f780b9fe15220997e19adec22d22565021457
Michał Borowiecki <mihbor@users.noreply.github.com>
2022-11-23 21:28:06 +0000

    Publish package to GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,3 +121,16 @@ android {
     targetCompatibility = JavaVersion.VERSION_11
   }
 }
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/mihbor/MinimaK")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,5 @@ pluginManagement {
         }
     }
 }
-rootProject.name = "MinimaK"
+rootProject.name = "minimak"
 
