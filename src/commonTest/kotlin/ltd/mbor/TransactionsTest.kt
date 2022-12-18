9981e38228941d00c7f738386c82c52c8a4b0d99
Michał Borowiecki <mihbor@users.noreply.github.com>
2022-12-18 15:21:54 +0000

    unit test ofAtLeast function

---
+++ src/commonTest/kotlin/ltd/mbor/TransactionsTest.kt
@@ -0,0 +1,41 @@
+package ltd.mbor
+
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import com.ionspin.kotlin.bignum.decimal.BigDecimal.Companion.ONE
+import com.ionspin.kotlin.bignum.decimal.BigDecimal.Companion.TEN
+import ltd.mbor.minimak.Coin
+import ltd.mbor.minimak.ofAtLeast
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TransactionsTest {
+  @Test
+  fun Given_1_coin_ofAtLeast_returns_it() {
+    for (given in listOf(coin(ONE), coin(TEN))) {
+      val result = listOf(given).ofAtLeast(ONE)
+      assertEquals(listOf(given), result)
+    }
+  }
+  @Test
+  fun Given_2_coins_ofAtLeast_returns_smallest_sufficient() {
+    val given = listOf(coin(ONE), coin(TEN))
+    assertEquals(listOf(coin(ONE)), given.ofAtLeast(ONE))
+    assertEquals(listOf(coin(TEN)), given.ofAtLeast(ONE + ONE))
+  }
+  @Test
+  fun Given_2_coins_ofAtLeast_returns_both_when_needed() {
+    val given = listOf(coin(ONE), coin(TEN))
+    assertEquals(setOf(coin(ONE), coin(TEN)), given.ofAtLeast(TEN + ONE).toSet())
+  }
+  
+  private fun coin(tokenAmount: BigDecimal) = Coin(
+    address = "",
+    miniAddress = "",
+    amount = tokenAmount,
+    coinId = "",
+    storeState = false,
+    tokenId = "",
+    _created = "",
+    state = emptyList()
+  )
+}
